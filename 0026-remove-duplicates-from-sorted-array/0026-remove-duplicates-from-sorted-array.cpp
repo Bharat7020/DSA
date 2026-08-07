7800a449e378e635868089e3a15e695207e53aef
Bharat Baban Kamble <166193364+Bharat7020@users.noreply.github.com>
2026-08-07 23:01:22 +0530

    Time: 0 ms (100%), Space: 22.7 MB (19.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,8 +5,8 @@ public:
         int n = nums.size();
         for(int j =1;j<n;j++){
             if(nums[i] != nums[j]){
-                nums[i+1] = nums[j];
                 i++;
+                nums[i] = nums[j];
             }
             
         }
